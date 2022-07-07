3187de7ee90d73ec4c6a7f47f360911573d9f012
hab <73842931+moha-b@users.noreply.github.com>
2022-07-07 18:49:40 +0200

    updates
    edit on login fragment add new drawers new colors new styles

---
+++ app/src/main/java/com/udacity/shoestore/models/data.kt
@@ -0,0 +1,13 @@
+package com.udacity.shoestore.models
+
+import androidx.lifecycle.ViewModel
+
+class data: ViewModel() {
+    init {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
